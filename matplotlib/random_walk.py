be636e61bafac67c782aeef843ec611272b569e0
admin <1029804941@qq.com>
2017-08-09 11:54:29 +0800

    matplotlib

--- matplotlib/random_walk.py
+++ matplotlib/random_walk.py
@@ -4,15 +4,15 @@
 from random import choice
 
 class RandomWalk():
-     def __init__(self,num_point=5000):      #一个生成随机漫步数据的类
-             self.num_point=num_point         #初始化随机漫步的属性
+     def __init__(self,num_points=5000):      #一个生成随机漫步数据的类
+             self.num_points=num_points         #初始化随机漫步的属性
              #所有随机漫步都始于(0,0)
              self.x_values=[0]
              self.y_values=[0]
      #计算随机漫步包含的所有点
      def fill_walk(self):
      	       #不断漫步，直到列表到达指定的长度
-     	       while len(self.x_values)<self.num_point:
+     	       while len(self.x_values)<self.num_points:
                  #决定前进方向以及前进的距离
      	       	x_direction=choice([-1,1])
      	       	x_distance=choice([0,1,2,3,4])
--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -3,14 +3,22 @@
 
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
-
+#只要程序处于活动状态，就不断的模拟随机漫步
 while True:
-      rw=RandomWalk()
+      rw=RandomWalk(50000)   #创建一个RandomWalk实例，将其包含的点都绘制出来
       rw.fill_walk()
-      plt.scatter(rw.x_values,rw.y_values,s=15)
-      #plt.savefig('rw_visual.png',bbox_inches='tight')
+
+      point_numbers=list(range(rw.num_points))
+      plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolor='none',s=1)
+
+      plt.scatter(0,0,c='red',edgecolor='none',s=10)  #起点
+      plt.scatter(rw.x_values[-1],rw.y_values[-1],c='yellow',edgecolor='none',s=10) #终点
+
+      plt.axes().get_xaxis().set_visible(False)
+      plt.axes().get_yaxis().set_visible(False)
+      plt.savefig('rw_visual.png',bbox_inches='tight')
       plt.show()
 
-      keep_running=raw_input("Make another walk? (y/n):")
+      keep_running=raw_input("Make another walk? (y/n):")   #input on python3
       if keep_running=='n':
       	break
