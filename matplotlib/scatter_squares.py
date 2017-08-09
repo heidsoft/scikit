b41e7a108a4a93f274561d73fb321e77192d2743
admin <1029804941@qq.com>
2017-08-09 10:25:46 +0800

    matplotlib

--- matplotlib/scatter_squares.py
+++ matplotlib/scatter_squares.py
@@ -20,7 +20,7 @@ plt.scatter(x_values,y_values,s=100)
 x_values=list(range(1,1001))
 y_values=[x**2 for x in x_values]
 
-plt.scatter(x_values,y_values,s=40)
+plt.scatter(x_values,y_values,c=y_values,cmap=plt.cm.Blues,edgecolor='none',s=40)
 
 plt.axis([0,1100,0,1100000])
 
