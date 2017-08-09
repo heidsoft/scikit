9dd8cecc96307dd483732320dca0cff18231347d
admin <1029804941@qq.com>
2017-08-09 11:32:33 +0800

    matplotlib

--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -4,7 +4,13 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw=RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values,rw.y_values,s=15)
-plt.savefig('rw_visual.png',bbox_inches='tight')
+while True:
+      rw=RandomWalk()
+      rw.fill_walk()
+      plt.scatter(rw.x_values,rw.y_values,s=15)
+      #plt.savefig('rw_visual.png',bbox_inches='tight')
+      plt.show()
+
+      keep_running=raw_input("Make another walk? (y/n):")
+      if keep_running=='n':
+      	break
